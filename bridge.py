ead7e46a5c8d733208d4352bf1a667bf00fdaa7c
Murilo Lima <54185216+omurilolima@users.noreply.github.com>
2024-04-09 19:41:39 +0100

    Bridge Design Pattern

---
+++ bridge.py
@@ -0,0 +1,37 @@
+class DrawingAPIOne(object):
+    """ Imprementation-specific abstraction: concrete class one """
+    def draw_circle(self, x, y, radius):
+        print(f"API 1 draing a circle at ({x}, {y} with radius {radius})!")
+
+class DrawingAPITwo(object):
+    """ Imprementation-specific abstraction: concrete class two"""
+    def draw_circle(self, x, y, radius):
+        print(f"API 2 draing a circle at ({x}, {y} with radius {radius})!")
+
+class Circle(object):
+    """ Implementation-independent abstraction: for exemple, there could """
+
+    def __init__(self, x, y, radius, drawing_api):
+        """ Initialize the necessary attributes """
+        self._x = x
+        self._y = y
+        self._radius = radius
+        self._drawing_api = drawing_api
+
+    def draw(self):
+        """ Implementation-specific abstraction taken care of by another """
+        self._drawing_api.draw_circle(self._x, self._y, self._radius)
+    
+    def scale(self, percent):
+        """ Implementation-independent """
+        self._radius *= percent
+    
+# Build the first Circle object using API One
+circle1 = Circle(1, 2, 3, DrawingAPIOne())
+# Dray a circle
+circle1.draw()
+
+# Build the second Circle object using API Two
+circle2 = Circle(2, 3, 4, DrawingAPITwo())
+# Draw a circle
+circle2.draw()
